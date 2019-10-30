757d713ca943418072a64f341f4c4d9b155a0d10
Egbert Rijke <e.m.rijke@gmail.com>
2019-10-31 00:08:24 +0100

    implementing andrejs suggestions

--- Coq/nat.v
+++ Coq/nat.v
@@ -13,11 +13,15 @@ Fixpoint add_NN (m n : NN) :=
 Definition add_NN' (m n : NN) : NN :=
   add_NN n m.
 
+Notation "x '+' y" := (add_NN x y).
+
 Fixpoint mul_NN (m n : NN) : NN :=
   match n with
   | zero => zero
   | succ n => add_NN m (mul_NN m n)
   end.
+
+Notation "x '*' y" := (mul_NN x y).
 
 Fixpoint fibonacci (n : NN) : NN :=
   match n with 
@@ -40,140 +44,3 @@ Fixpoint power (m n : NN) : NN :=
   | zero => succ zero
   | succ m => mul_NN n (power m n)
   end.
-
-Eval compute in fibonacci (succ (succ (succ (succ (succ (succ zero)))))).
-                                
-
-Eval compute in mul_NN (succ (succ zero)) (succ (succ zero)).
-
-Inductive Id {A} (a : A) : A -> Type :=
-| refl : Id a a.
-
-Lemma inv {A} {x y : A} (p : Id x y) : Id y x.
-Proof.
-  destruct p.
-  apply refl.
-Defined.
-
-Lemma concat {A} {x y z : A} (p : Id x y) (q : Id y z) : Id x z.
-Proof.
-  destruct p.
-  assumption.
-Defined.
-
-Lemma ap {A B} (f : A -> B) {x y : A} (p : Id x y) : Id (f x) (f y).
-Proof.
-  destruct p.
-  apply refl.
-Defined.
-
-Definition left_unit_law_add_NN (n : NN) : Id (add_NN zero n) n.
-Proof.
-  induction n.
-  - apply refl.
-  - cbn. now apply ap.
-Defined.
-
-Definition right_unit_law_add_NN (n : NN) : Id (add_NN n zero) n.
-Proof.
-  reflexivity.
-Defined.
-
-Definition left_successor_law_add_NN (m n : NN) :
-  Id (add_NN (succ m) n) (succ (add_NN m n)).
-Proof.
-  induction n.
-  - reflexivity.
-  - apply (ap succ). assumption.
-Defined.
-
-Definition right_successor_law_add_NN (m n : NN) :
-  Id (add_NN m (succ n)) (succ (add_NN m n)).
-Proof.
-  reflexivity.
-Defined.
-
-Fixpoint associative_add_NN (m n k : NN) :
-  Id (add_NN (add_NN m n) k) (add_NN m (add_NN n k)) :=
-  match k with
-  | zero => refl _
-  | succ k => ap succ (associative_add_NN m n k)
-  end.
-
-Definition commutative_add_NN (m n : NN) :
-  Id (add_NN m n) (add_NN n m).
-Proof.
-  induction n as [|n commutative_add_NN_m_n].
-  - apply inv.
-    apply left_unit_law_add_NN.
-  - eapply concat.
-    * exact (ap succ commutative_add_NN_m_n).
-    * apply inv. apply left_successor_law_add_NN.
-Defined.
-
-Definition left_zero_law_mul_NN (n : NN) :
-  Id (mul_NN zero n) zero.
-Proof.
-  induction n.
-  - reflexivity.
-  - eapply concat.
-    * apply left_unit_law_add_NN.
-    * assumption.
-Defined.
-
-Definition right_zero_law_mul_NN (n : NN) :
-  Id (mul_NN n zero) zero.
-Proof. reflexivity. Defined.
-
-Definition left_successor_law_mul_NN (m n : NN) :
-  Id (mul_NN (succ m) n) (add_NN (mul_NN m n) n).
-Proof.
-  induction n as [|n left_successor_law_mul_NN_m_n].
-  - reflexivity.
-  - eapply concat.
-    * exact (ap (add_NN (succ m)) left_successor_law_mul_NN_m_n).
-    * eapply concat.
-      ** apply left_successor_law_add_NN.
-      ** apply (ap succ). apply inv. apply associative_add_NN.
-Defined.
-
-Definition right_successor_law_mul_NN (m n : NN) :
-  Id (mul_NN m (succ n)) (add_NN m (mul_NN m n)).
-Proof. reflexivity. Defined.
-
-Definition left_distributive_mul_add_NN (m n k : NN) :
-  Id (mul_NN m (add_NN n k)) (add_NN (mul_NN m n) (mul_NN m k)).
-Proof.
-  induction k as [|k left_distributive_mul_add_NN_m_n_k].
-  - reflexivity.
-  - eapply concat.
-    * exact (ap (add_NN m) left_distributive_mul_add_NN_m_n_k).
-    * eapply concat.
-      ** apply inv. apply associative_add_NN.
-      ** eapply concat.
-         *** exact
-               (ap (add_NN' (mul_NN m k)) (commutative_add_NN m (mul_NN m n))).
-         *** apply associative_add_NN.
-Defined.
-    
-Definition associative_mul_NN (m n k : NN) :
-  Id (mul_NN (mul_NN m n) k) (mul_NN m (mul_NN n k)).
-Proof.
-  induction k as [|k associative_mul_NN_m_n_k].
-  - reflexivity.
-  - eapply concat.
-    * exact (ap (add_NN (mul_NN m n)) associative_mul_NN_m_n_k).
-    * apply inv. apply left_distributive_mul_add_NN.
-Defined.
-
-Definition commutative_mul_NN (m n : NN) :
-  Id (mul_NN m n) (mul_NN n m).
-Proof.
-  induction n as [|n commutative_mul_NN_m_n].
-  - apply inv. apply left_zero_law_mul_NN.
-  - eapply concat.
-    * apply commutative_add_NN.
-    * eapply concat.
-      ** exact (ap (add_NN' m) commutative_mul_NN_m_n).
-      ** apply inv. apply left_successor_law_mul_NN.
-Defined.
--- Coq/pi.v
+++ Coq/pi.v
@@ -1,5 +1,31 @@
+Definition idmap {A} : A -> A.
+Proof.
+  intro a.
+  exact a.
+Defined.
+
 Definition comp {A B C : Type} (g : B -> C) (f : A -> B) : A -> C.
 Proof.
-  intros a.
+  intro a.
   exact (g (f a)).
+Defined.
+
+Definition const {A B} (a : A) : B -> A.
+Proof.
+  intro b.
+  exact a.
+Defined.
+
+Definition comp_Pi {A} {B : A -> Type} {C : forall x, B x -> Type} :
+  forall (f : forall x, B x), (forall x y, C x y) -> (forall x, C x (f x)).
+Proof.
+  intros f g x.
+  exact (g x (f x)).
+Defined.
+
+Definition swap_Pi {A B} {C : A -> (B -> Type)} :
+  (forall x y, C x y) -> (forall y x, C x y).
+Proof.
+  intros h b a.
+  exact (h a b).
 Defined.
