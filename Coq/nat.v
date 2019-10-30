902724b04d86d47053920944ce4d091935f100cb
Egbert Rijke <e.m.rijke@gmail.com>
2019-10-30 20:40:53 +0100

    multiplication on N is commutative

--- Coq/nat.v
+++ Coq/nat.v
@@ -10,6 +10,9 @@ Fixpoint add_NN (m n : NN) :=
   | succ n => succ (add_NN m n)
   end.
 
+Definition add_NN' (m n : NN) : NN :=
+  add_NN n m.
+
 Fixpoint mul_NN (m n : NN) : NN :=
   match n with
   | zero => zero
@@ -107,4 +110,70 @@ Proof.
     * exact (ap succ commutative_add_NN_m_n).
     * apply inv. apply left_successor_law_add_NN.
 Defined.
-  
+
+Definition left_zero_law_mul_NN (n : NN) :
+  Id (mul_NN zero n) zero.
+Proof.
+  induction n.
+  - reflexivity.
+  - eapply concat.
+    * apply left_unit_law_add_NN.
+    * assumption.
+Defined.
+
+Definition right_zero_law_mul_NN (n : NN) :
+  Id (mul_NN n zero) zero.
+Proof. reflexivity. Defined.
+
+Definition left_successor_law_mul_NN (m n : NN) :
+  Id (mul_NN (succ m) n) (add_NN (mul_NN m n) n).
+Proof.
+  induction n as [|n left_successor_law_mul_NN_m_n].
+  - reflexivity.
+  - eapply concat.
+    * exact (ap (add_NN (succ m)) left_successor_law_mul_NN_m_n).
+    * eapply concat.
+      ** apply left_successor_law_add_NN.
+      ** apply (ap succ). apply inv. apply associative_add_NN.
+Defined.
+
+Definition right_successor_law_mul_NN (m n : NN) :
+  Id (mul_NN m (succ n)) (add_NN m (mul_NN m n)).
+Proof. reflexivity. Defined.
+
+Definition left_distributive_mul_add_NN (m n k : NN) :
+  Id (mul_NN m (add_NN n k)) (add_NN (mul_NN m n) (mul_NN m k)).
+Proof.
+  induction k as [|k left_distributive_mul_add_NN_m_n_k].
+  - reflexivity.
+  - eapply concat.
+    * exact (ap (add_NN m) left_distributive_mul_add_NN_m_n_k).
+    * eapply concat.
+      ** apply inv. apply associative_add_NN.
+      ** eapply concat.
+         *** exact
+               (ap (add_NN' (mul_NN m k)) (commutative_add_NN m (mul_NN m n))).
+         *** apply associative_add_NN.
+Defined.
+    
+Definition associative_mul_NN (m n k : NN) :
+  Id (mul_NN (mul_NN m n) k) (mul_NN m (mul_NN n k)).
+Proof.
+  induction k as [|k associative_mul_NN_m_n_k].
+  - reflexivity.
+  - eapply concat.
+    * exact (ap (add_NN (mul_NN m n)) associative_mul_NN_m_n_k).
+    * apply inv. apply left_distributive_mul_add_NN.
+Defined.
+
+Definition commutative_mul_NN (m n : NN) :
+  Id (mul_NN m n) (mul_NN n m).
+Proof.
+  induction n as [|n commutative_mul_NN_m_n].
+  - apply inv. apply left_zero_law_mul_NN.
+  - eapply concat.
+    * apply commutative_add_NN.
+    * eapply concat.
+      ** exact (ap (add_NN' m) commutative_mul_NN_m_n).
+      ** apply inv. apply left_successor_law_mul_NN.
+Defined.
