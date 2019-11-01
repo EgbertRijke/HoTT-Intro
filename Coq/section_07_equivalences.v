017656bc3b1d27b9283f7eb139dd0b60f77f7fa8
Egbert Rijke <e.m.rijke@gmail.com>
2019-11-01 20:57:13 +0100

    up to exercise 7.3

--- Coq/section_07_equivalences.v
+++ Coq/section_07_equivalences.v
@@ -113,7 +113,7 @@ Notation "A '<~>' B" := (equiv A B) (at level 70).
 
 Definition map_equiv {A B} (e : A <~> B) : A -> B := pr1 e.
 
-Definition is_equiv_equiv_map_equiv {A B} (e : A <~> B) :
+Definition is_equiv_map_equiv {A B} (e : A <~> B) :
   is_equiv (map_equiv e) := pr2 e.
 
 (** Remark 7.2.2 *)
@@ -121,8 +121,10 @@ Definition is_equiv_equiv_map_equiv {A B} (e : A <~> B) :
 Definition has_inverse {A B} (f : A -> B) : Type :=
   Sigma (B -> A) (fun g => prod (comp f g ~ idmap) (comp g f ~ idmap)).
 
-Definition is_equiv_has_inverse {A B} {f : A -> B} (g : B -> A) (G : comp f g ~ idmap) (H : comp g f ~ idmap) : is_equiv f.
+Definition is_equiv_has_inverse {A B} {f : A -> B} :
+  forall (g : B -> A), (comp f g ~ idmap) -> (comp g f ~ idmap) -> is_equiv f.
 Proof.
+  intros g G H.
   exact (pair (pair g G) (pair g H)).
 Defined.
 
@@ -198,12 +200,21 @@ Proof.
   apply has_inverse_inv_is_equiv.
 Defined.
 
+Definition inv_equiv {A B} : (A <~> B) -> (B <~> A).
+Proof.
+  intro e.
+  apply (pair (inv_is_equiv (is_equiv_map_equiv e))).
+  apply is_equiv_inv_is_equiv.
+Defined.
+
 (** Remark 7.2.5 *)
 
 Definition is_equiv_idmap {A} : is_equiv (@idmap A).
 Proof.
   apply (is_equiv_has_inverse idmap); exact refl_htpy.
 Defined.
+
+Definition id_equiv {A} : A <~> A := pair idmap is_equiv_idmap.
 
 (** Example 7.2.6 *)
 
@@ -215,3 +226,253 @@ Proof.
 Defined.
 
 (** Section 7.3. The identity type of a Sigma-type *)
+
+(** Definition 7.3.1 *)
+
+Definition Eq_Sigma {A : Type} {B : A -> Type} (s t : Sigma A B) : Type :=
+  Sigma (pr1 s == pr1 t) (fun p => tr B p (pr2 s) == pr2 t).
+
+(** Lemma 7.3.2 *)
+
+Lemma refl_Eq_Sigma {A B} {s : Sigma A B} : Eq_Sigma s s.
+Proof.
+  exact (pair refl refl).
+Defined.
+
+(** Definition 7.3.3 *)
+
+Definition pair_eq {A B} {s t : Sigma A B} (p : s == t) : Eq_Sigma s t.
+Proof.
+  destruct p.
+  exact refl_Eq_Sigma.
+Defined.
+
+(** Theorem 7.3.4 *)
+
+Definition eq_pair {A B} {s t : Sigma A B} (p : Eq_Sigma s t) : s == t.
+Proof.
+  destruct p as [p q].
+  destruct s as [x y]; destruct t as [x' y'].
+  cbn in p; cbn in q.
+  destruct p; destruct q.
+  reflexivity.
+Defined.
+
+Definition is_sec_eq_pair {A B} {s t : Sigma A B} :
+  comp (@pair_eq A B s t) (@eq_pair A B s t) ~ idmap.
+Proof.
+  intro p.
+  destruct p as [p q].
+  destruct s as [x y]; destruct t as [x' y'].
+  cbn in p; cbn in q.
+  destruct p; destruct q.
+  reflexivity.
+Defined.
+
+Definition is_retr_eq_pair {A B} {s t : Sigma A B} :
+  comp (@eq_pair A B s t) (@pair_eq A B s t) ~ idmap.
+Proof.
+  intro p; destruct p.
+  destruct s. reflexivity.
+Defined.
+
+Theorem is_equiv_pair_eq {A B} {s t : Sigma A B} :
+  is_equiv (@pair_eq A B s t).
+Proof.
+  apply (is_equiv_has_inverse eq_pair).
+  - exact is_sec_eq_pair.
+  - exact is_retr_eq_pair.
+Defined.
+
+Definition pair_eq_equiv {A B} {s t : Sigma A B} :
+  (s == t) <~> Eq_Sigma s t := pair pair_eq is_equiv_pair_eq.
+
+Theorem is_equiv_eq_pair {A B} {s t : Sigma A B} :
+  is_equiv (@eq_pair A B s t).
+Proof.
+  apply (is_equiv_has_inverse pair_eq).
+  - exact is_retr_eq_pair.
+  - exact is_sec_eq_pair.
+Defined.
+
+Definition eq_pair_equiv {A B} {s t : Sigma A B} :
+  (Eq_Sigma s t) <~> (s == t) := pair eq_pair is_equiv_eq_pair.
+
+(** Exercises *)
+
+(** Exercise 7.1 *)
+
+Definition inv_inv {A} {x y : A} (p : x == y) : inv (inv p) == p.
+Proof.
+  now destruct p.
+Defined.
+
+Theorem is_equiv_inv {A} {x y} : is_equiv (@inv A x y).
+Proof.
+  apply (is_equiv_has_inverse inv); exact inv_inv.
+Defined.
+
+Definition invmap_equiv {A} {x y : A} : (x == y) <~> (y == x) :=
+  pair inv is_equiv_inv.
+
+Theorem is_equiv_concat {A} {x y z : A} (p : x == y) :
+  is_equiv (@concat A x y z p).
+Proof.
+  apply (is_equiv_has_inverse (concat (inv p))); destruct p; exact refl_htpy.
+Defined.
+
+Definition concat_equiv {A} {x y z : A} (p : x == y) :
+  (y == z) <~> (x == z) :=
+  pair (concat p) (is_equiv_concat p).
+
+Theorem is_equiv_concat' {A} {x y z : A} (q : y == z) :
+  is_equiv (@concat' A x y z q).
+Proof.
+  apply (is_equiv_has_inverse (concat' (inv q)));
+    destruct q; intro p; now destruct p.
+Defined.
+
+Definition concat_equiv' {A} {x y z : A} (q : y == z) :
+  (x == y) <~> (x == z) :=
+  pair (concat' q) (is_equiv_concat' q).
+
+Theorem is_equiv_tr {A} {B : A -> Type} {x y} (p : x == y) : is_equiv (tr B p).
+Proof.
+  apply (is_equiv_has_inverse (tr B (inv p))); destruct p; exact refl_htpy.
+Defined.
+
+Definition tr_equiv {A} (B : A -> Type) {x y} (p : x == y) :
+  B x <~> B y := pair (tr B p) (is_equiv_tr p).
+
+(** Exercise 7.2 *)
+
+Definition inv_inl {X} : coprod X empty -> X.
+Proof.
+  intro x; now destruct x.
+Defined.
+
+Definition is_sec_inv_inl {X} : comp (@inl X empty) (@inv_inl X) ~ idmap.
+Proof.
+  intro x; now destruct x.
+Defined.
+
+Definition is_retr_inv_inl {X} : comp (@inv_inl X) (@inl X empty) ~ idmap.
+Proof.
+  now intro x.
+Defined.
+
+Theorem is_equiv_inl {X} : is_equiv (@inl X empty).
+Proof.
+  apply (is_equiv_has_inverse (inv_inl)).
+  - exact is_sec_inv_inl.
+  - exact is_retr_inv_inl.
+Defined.
+
+Definition inv_inr {X} : coprod empty X -> X.
+Proof.
+  intro x; now destruct x.
+Defined.
+
+Definition is_sec_inv_inr {X} : comp (@inr empty X) (@inv_inr X) ~ idmap.
+Proof.
+  intro x; now destruct x.
+Defined.
+
+Definition is_retr_inv_inr {X} : comp (@inv_inr X) (@inr empty X) ~ idmap.
+Proof.
+  now intro x.
+Defined.
+
+Theorem is_equiv_inr {X} : is_equiv (@inr empty X).
+Proof.
+  apply (is_equiv_has_inverse inv_inr).
+  - exact is_sec_inv_inr.
+  - exact is_retr_inv_inr.
+Defined.
+
+Definition inv_pr1_empty {X} : empty -> prod empty X := ex_falso.
+
+Definition is_sec_inv_pr1_empty {X} :
+  comp pr1 (@inv_pr1_empty X) ~ idmap := ex_falso.
+
+Definition is_retr_inv_pr1_empty {X} :
+  comp (@inv_pr1_empty X) pr1 ~ idmap.
+Proof.
+  intro x; now destruct x.
+Defined.
+
+Theorem is_equiv_pr1_empty {X} : is_equiv (@pr1 empty (fun t => X)).
+Proof.
+  apply (is_equiv_has_inverse (inv_pr1_empty)).
+  - exact is_sec_inv_pr1_empty.
+  - exact is_retr_inv_pr1_empty.
+Defined.
+
+Definition inv_pr2_empty {X} : empty -> prod X empty := ex_falso.
+
+Definition is_sec_inv_pr2_empty {X} :
+  comp (@pr2 X (const empty)) (@inv_pr2_empty X) ~ idmap := ex_falso.
+
+Definition is_retr_inv_pr2_empty {X} :
+  comp (@inv_pr2_empty X) (@pr2 X (const empty)) ~ idmap.
+Proof.
+  intro x. now destruct x.
+Defined.
+
+Theorem is_equiv_pr2_empty {X} : is_equiv (@pr2 X (const empty)).
+Proof.
+  apply (is_equiv_has_inverse inv_pr2_empty).
+  - exact is_sec_inv_pr2_empty.
+  - exact is_retr_inv_pr2_empty.
+Defined.
+
+(** Exercise 7.3 *)
+
+(** Exercise 7.3.a *)
+
+Definition is_sec_inv_is_equiv_htpy {A B} {f g : A -> B} (H : f ~ g)
+           (is_equiv_g : is_equiv g) :
+  comp f (inv_is_equiv is_equiv_g) ~ idmap :=
+  concat_htpy
+    (right_whisker_htpy H (inv_is_equiv is_equiv_g))
+    (is_sec_inv_is_equiv is_equiv_g).
+
+Definition is_retr_inv_is_equiv_htpy {A B} {f g : A -> B} (H : f ~ g)
+           (is_equiv_g : is_equiv g) :
+  comp (inv_is_equiv is_equiv_g) f ~ idmap :=
+  concat_htpy
+    (left_whisker_htpy (inv_is_equiv is_equiv_g) H)
+    (is_retr_inv_is_equiv is_equiv_g).
+
+Theorem is_equiv_htpy {A B} {f g : A -> B} (H : f ~ g) :
+  is_equiv g -> is_equiv f.
+Proof.
+  intro is_equiv_g.
+  apply (is_equiv_has_inverse (inv_is_equiv is_equiv_g)).
+  - exact (is_sec_inv_is_equiv_htpy H is_equiv_g).
+  - exact (is_retr_inv_is_equiv_htpy H is_equiv_g).
+Defined.
+
+Definition is_equiv_htpy' {A B} {f g : A -> B} (H : f ~ g) :
+  is_equiv f -> is_equiv g := is_equiv_htpy (inv_htpy H).
+
+(** Exercise 7.3.b *)
+
+Definition htpy_equiv {A B} (e e' : A <~> B) : Type :=
+  map_equiv e ~ map_equiv e'.
+
+Definition htpy_inv_equiv {A B} {e e' : A <~> B} :
+  htpy_equiv e e' -> htpy_equiv (inv_equiv e) (inv_equiv e').
+Proof.
+  intros H y.
+  transitivity
+    (map_equiv (inv_equiv e') (map_equiv e' (map_equiv (inv_equiv e) y))).
+  - apply inv. apply (is_retr_inv_is_equiv (is_equiv_map_equiv e')).
+  - transitivity
+      (map_equiv (inv_equiv e') (map_equiv e (map_equiv (inv_equiv e) y))).
+    * apply inv.
+      apply (ap (map_equiv (inv_equiv e'))).
+      apply H.
+    * apply (ap (map_equiv (inv_equiv e'))).
+      apply (is_sec_inv_is_equiv (is_equiv_map_equiv e)).
+Defined.
